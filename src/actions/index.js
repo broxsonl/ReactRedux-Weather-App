013b811bcd47bb719a0c9991e571b6d7da458a82
Lee Broxson <broxsonl@gmail.com>
2017-01-08 16:19:21 -0800

    Created fetch weather action, and created dynamic query string for user to input cities Added Axios to project.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,16 @@
 'use strict';
 
 const API_KEY = require('../../env.js');
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+// Story tyhe action type in a var to export it so it's consistent both here and where it's used elsewhere.
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  return (
+    // Action FETCH_WEATHER.
+    type: FETCH_WEATHER
+  );
+}
