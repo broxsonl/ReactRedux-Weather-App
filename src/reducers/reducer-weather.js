780b09b8cb9c618ba05ffa3c1ca5401bb3039a64
Lee Broxson <broxsonl@gmail.com>
2017-01-08 17:32:48 -0800

    Weather Reducer set to create new state for each city searched for by the user.

--- src/reducers/reducer-weather.js
+++ src/reducers/reducer-weather.js
@@ -1,5 +1,19 @@
 'use strict';
 
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+
+  if (action.type === FETCH_WEATHER) {
+    // Only taking back the data property on the payload return
+    // that we get. Don't need the rest.
+    // We are adding the data for each city called to the
+    // existing state array. This adds to the existing state,
+    // rather than mutating it.
+    // ES6 way of creating new array below. This also adds a new
+    // entry to the top of the array.
+    return [action.payload.data, ...state];
+  }
+
   return state;
 }
