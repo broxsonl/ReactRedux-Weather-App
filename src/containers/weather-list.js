6d8d999e5984e52b8a78aade6514fa59e4e6e72c
Lee Broxson <broxsonl@gmail.com>
2017-01-12 23:30:39 -0800

    Worked to try and fix chart sizing.

--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -3,16 +3,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import Chart from '../components/chart';
+
 class WeatherList extends Component {
 
   renderWeather(cityData) {
-
     let name = cityData.city.name;
+    let temps = cityData.list.map(weather => weather.main.temp);
+    let humidities = cityData.list.map(weather => weather.main.humidity);
+    let pressures = cityData.list.map(weather => weather.main.pressure);
 
     return (
       <tr key={name}>
         <td>{name}</td>
-        
+        <Chart data={temps} color="red" units="K" />
+        <Chart data={humidities} color="blue" units="hPa" />
+        <Chart data={pressures} color="green" units="%" />
       </tr>
     )
   }
@@ -23,9 +29,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Humidity (hPa)</th>
+            <th>Pressure (%)</th>
           </tr>
         </thead>
         <tbody>
